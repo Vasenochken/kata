bf2f845c65bc63b2d1d0cb2583cb495e06f61e44
Vaseliy <bonanava@yandex.ru>
2022-11-16 14:22:00 +0300

    done_block_320px

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,35 @@
 import "@babel/polyfill";
 import '../index.html';
 import '../scss/index.scss';
+import '../scss/swiper/swiper.scss';
+
+
+  // import Swiper JS
+  // import Swiper styles
+//   import 'swiper/css';
+//   import 'swiper/css/navigation';
+//   import 'swiper/css/pagination';
+
+  import Swiper, { Navigation, Pagination } from 'swiper';
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 'auto',
+    // autoHeight: true,
+    loop: true,
+    spaceBetween: 16,
+    slidesPerGroup: 1,
+    // If we need pagination
+    modules: [Navigation, Pagination],
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      type: 'bullets',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
